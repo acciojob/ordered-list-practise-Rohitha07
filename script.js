b534ea84e7223f60ec9ae8af1d6529d611decd9a
Rohitha Tudicherla <87955445+Rohitha07@users.noreply.github.com>
2023-08-17 09:11:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Function to optimize a program
+function optimizeProgram() {
+    // Code optimization techniques here
+    console.log("Program optimized successfully!");
+}
+
+// Call the optimizeProgram function
+optimizeProgram();
